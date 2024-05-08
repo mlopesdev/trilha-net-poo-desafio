7a84698f7d0c87fce062924787dc539755ceb89f
mlopesdev <152744110+mlopesdev@users.noreply.github.com>
2024-05-08 18:01:58 -0300

    Add files via upload

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,13 +3,46 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        private string _modelo;
+
+        private string _imei;
+
+        private int _memoria;
+
+        public string Modelo
         {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            get => _modelo;
+         
+            set
+            {
+                _modelo = value;
+            }
         }
+
+        public string Imei
+        {
+            get => _imei;
+         
+            set
+            {
+                _imei = value;
+            }
+        }
+
+        public int Memoria
+        {
+            get => _memoria;
+         
+            set
+            {
+                _memoria = value;
+            }
+        }
+
+        
+
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public void Ligar()
         {
